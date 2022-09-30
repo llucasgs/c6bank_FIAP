f6aceb26b2e2fe6596d122ea758c4ac5fd2d378a
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2022-09-30 16:06:49 -0300

    'Deploy'

---
+++ assets/js/tooltip.js
@@ -0,0 +1,34 @@
+const tooltip = document.querySelector("#tooltip");
+const body = document.querySelector("body");
+const header = document.querySelector(".top-site");
+
+// quem é o target?
+// quem.addEventListener("evento", (evento)=> {});
+
+tooltip.addEventListener("mouseover",(e) => {
+    console.log(e);
+    console.log('Altura da página', body.clientHeight);
+    console.log('largura da página', body.clientWidth);
+    console.log('Altura do tooltip', tooltip.clientHeight);
+    console.log('largura do tooltip', tooltip.clientWidth);
+    console.log('Altura do mouse', e.clientY);
+    console.log('largura do mouse', e.clientX);
+
+    if(document.querySelector(".tooltip")) {
+        document.querySelector(".tooltip").remove();
+    }
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("tooltip");
+        newDiv.innerHTML = "Meu tooltip com muitas dicas tops demais, aparece e desaparece.";
+    newDiv.style.position = "absolute";
+    newDiv.style.top = e.clientY + "px";
+    newDiv.style.left = (e.clientX + 200 > body.clientWidth ? e.clientX -210 : e.clientX + 200) + "px";
+    newDiv.style.display = "flex";
+    header.appendChild(newDiv);
+});
+
+tooltip.addEventListener("mouseleave", (e) => {
+    if(document.querySelector(".tooltip")) {
+        document.querySelector(".tooltip").remove();
+    }
+});
